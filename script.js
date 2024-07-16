e3aaf01e3a6eb067284a89939b904c7f5c8c0a8e
Jkdev269 <jaharuddin269@gmail.com>
2024-07-16 23:56:05 +0530

    -

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-const express=require('express')
+const express=require('express');
+const serveStatic = require('serve-static');
+const path = require('path');
 const app=express();
 app.use(express.static('public'))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'first.html'));
+});
+app.get('/api/data', (req, res) => {
+    res.json({ message: 'Hello, this is your data!' });
+});
 app.use((req,res)=>{
-    res.status(404)
+
+    
+    res.status(200)
     res.send(`<h1>Error 404!!! Page Not Found</h1>`)
 
 })
